d285cef2ac187f4ccac84053f06f7f17d44ab04e
pulgamecanica <pulgamecanica11@gmail.com>
2023-04-24 00:31:36 +0100

    First Unit Test And fix Actions

--- backend/portfolio42_api/tests.py
+++ backend/portfolio42_api/tests.py
@@ -1,3 +1,18 @@
-from django.test import TestCase
+import unittest
+from django.test import Client
 
-# Create your tests here.
+
+class SimpleTest(unittest.TestCase):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+    def test_details(self):
+        # Issue a GET request.
+        response = self.client.get("/not-found")
+
+        # Check that the response is 200 OK.
+        self.assertEqual(response.status_code, 404)
+
+        # Check that the rendered context contains 5 customers.
+        # self.assertEqual(len(response.context["customers"]), 5)
