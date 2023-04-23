c8973fa64967a6a49f4f5d6dfd75e79d0a2584d6
pulgamecanica <pulgamecanica11@gmail.com>
2023-04-23 19:29:43 +0100

    Start django REST api application

--- backend/portfolio42/settings.py
+++ backend/portfolio42/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'portfolio42_api',
 ]
 
 MIDDLEWARE = [
---
+++ backend/portfolio42_api/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class Portfolio42ApiConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'portfolio42_api'
