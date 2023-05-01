1a0c905e129f3a8b412cf3d5ebd36ba56293361f
pulgamecanica <pulgamecanica11@gmail.com>
2023-05-02 00:00:10 +0100

    Add Home, login and callback endpoints

--- backend/portfolio42_api/urls.py
+++ backend/portfolio42_api/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.api_home, name='api_home'),
+    path('auth/login', views.login, name='login'),
     path('auth/callback_intra', views.callback_intra, name='callback_intra'),
 ]
--- backend/portfolio42_api/views.py
+++ backend/portfolio42_api/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.http import HttpResponse
+"""
 
-# Create your views here.
+"""
+def	api_home(request):
+	return HttpResponse("<center><h1>Welcome to 42Api</h1></center>")
+
+def login(request):
+	if request.user.is_authenticated:
+		return redirect("api_home")
+	return redirect("https://api.intra.42.fr/oauth/authorize?client_id=u-s4t2ud-12ea5db76c55712f66929aac2d7b89e4199018d87ca91386c1f3bf7def515052&redirect_uri=http%3A%2F%2Flocalhost%3A3002%2Fapi%2Fauth%2Fcallback_intra&response_type=code")
+
 def callback_intra(request):
 	pass
