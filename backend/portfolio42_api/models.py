dceabcc82d8ba514eaee4fe9b5f0584fe361f42d
pulgamecanica <pulgamecanica11@gmail.com>
2023-05-03 13:53:07 +0100

    Remove bio

--- backend/portfolio42_api/models.py
+++ backend/portfolio42_api/models.py
@@ -12,7 +12,6 @@ class User(AbstractUser):
 	email = models.EmailField(max_length=130)
 	intra_url = models.CharField(max_length=200);
 	image_url = models.CharField(max_length=800);
-	bio = models.TextField(max_length=800)
 	updated_at = models.DateTimeField(auto_created=True, auto_now=True)
 	is_admin = models.BooleanField(default=False)
 
