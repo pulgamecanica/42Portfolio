5436d0bfc61a494a9fa1e14cc8951fba0b723531
Diederik Boeren <diederik.boeren@gmail.com>
2023-05-03 19:50:00 +0100

    Improved code style on models.py

--- backend/portfolio42_api/models.py
+++ backend/portfolio42_api/models.py
@@ -11,8 +11,8 @@ class User(AbstractUser):
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
 	email = models.EmailField(max_length=130)
-	intra_url = models.CharField(max_length=200);
-	image_url = models.CharField(max_length=800);
+	intra_url = models.CharField(max_length=200)
+	image_url = models.CharField(max_length=800)
 	updated_at = models.DateTimeField(auto_created=True, auto_now=True)
 	is_admin = models.BooleanField(default=False)
 
@@ -22,7 +22,7 @@ class User(AbstractUser):
 	def __str__(self):
 		return "@" + self.intra_username
 
-	def wasUpdatedToday(self):
+	def was_updated_today(self):
 		return self.updated_at > date.yesterday()
 
 	def serialize(self):
@@ -31,12 +31,12 @@ class User(AbstractUser):
 # Cursus model
 class Cursus(models.Model):
 	intra_id = models.IntegerField(unique=True, db_index=True)
-	name = models.stringField(max_length=50)
-	kind = models.stringField(max_length=50)
+	name = models.CharField(max_length=50)
+	kind = models.CharField(max_length=50)
 	updated_at = models.DateTimeField(auto_created=True, auto_now=True)
 
 	def __str__(self):
 		return f"cursus: {self.name}"
 
-	def wasUpdatedToday(self):
+	def was_updated_today(self):
 		return self.updated_at > date.yesterday()
