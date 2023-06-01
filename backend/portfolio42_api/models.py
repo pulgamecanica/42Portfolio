d61d9c8e63cca1281979f2e2d227a626a2bd3b43
PukieDiederik <39601570+PukieDiederik@users.noreply.github.com>
2023-06-01 09:49:34 +0100

    Added relations between our models (#27)
    * Added Project Cursus
    
    * Added CursusSkill
    
    * Added ProjectUser
    
    * Added ProjectUserCursus
    
    * Added CursusUser
    
    * Added CursusUserSkill
    
    * added migrations
    
    * Added intraid to projectUser
    
    * Removed old migrations
    
    * Added manytomany relations for django
    
    * Reverted last migration and created new one with changes. And removed ProjectUserCursus relation
    
    * added related_name to ManyToManyFields
    
    * Added migration

--- backend/portfolio42_api/models.py
+++ backend/portfolio42_api/models.py
@@ -16,6 +16,8 @@ class User(AbstractUser):
 	updated_at = models.DateTimeField(auto_created=True, auto_now=True)
 	is_admin = models.BooleanField(default=False)
 
+	cursus = models.ManyToManyField('Cursus', through='CursusUser', related_name='users')
+
 	REQUIRED_FIELDS = ["email", "intra_id", "first_name", "last_name", 'intra_url']
 	USERNAME_FIELD = "intra_username"
 
@@ -35,6 +37,8 @@ class Cursus(models.Model):
 	kind = models.CharField(max_length=50)
 	updated_at = models.DateTimeField(auto_created=True, auto_now=True)
 
+	skills = models.ManyToManyField('Skill', through='CursusSkill', related_name='cursus')
+
 	def __str__(self):
 		return f"{self.name}:{self.intra_id}"
 
@@ -49,6 +53,9 @@ class Project(models.Model):
 	exam = models.BooleanField(default=False)
 	solo = models.BooleanField(default=True)
 	updated_at = models.DateTimeField(auto_created=True, auto_now=True)
+
+	users = models.ManyToManyField('User', through='ProjectUser', related_name='projects')
+	cursus = models.ManyToManyField('cursus', through='ProjectCursus', related_name='projects')
 	
 	def __str__(self):
 		return f"{self.name}:{self.intra_id}"
@@ -67,3 +74,42 @@ class Skill(models.Model):
 
 	def was_updated_today(self):
 		return self.updated_at > date.yesterday()
+
+## Relations
+# These are supposed to link the rest of the models together
+
+# This is a project that a user has subscribed to
+class ProjectUser(models.Model):
+	id_user = models.ForeignKey('User', on_delete=models.CASCADE)
+	id_project = models.ForeignKey('Project', on_delete=models.CASCADE)
+	intra_id = models.IntegerField(unique=True, db_index=True)
+	grade = models.IntegerField()
+	finished = models.BooleanField(default=False)
+	finished_at = models.DateTimeField()
+	updated_at = models.DateTimeField(auto_created=True, auto_now=True)
+
+# This is a cursus a user is enrolled in
+class CursusUser(models.Model):
+	id_user = models.ForeignKey('User', on_delete=models.CASCADE)
+	id_cursus = models.ForeignKey('Cursus', on_delete=models.CASCADE)
+	level = models.FloatField()
+	intra_id = models.IntegerField(unique=True, db_index=True)
+	begin_at = models.DateField()
+
+# Creates a relation between a project and a cursus, it relates which projects are in a cursus
+class ProjectCursus(models.Model):
+	id_cursus = models.ForeignKey('Cursus', on_delete=models.CASCADE)
+	id_project = models.ForeignKey('Project', on_delete=models.CASCADE)
+
+# Creates a relation between a skill and a cursus, it relates which skills are part of a cursus
+class CursusSkill(models.Model):
+	id_cursus = models.ForeignKey('Cursus', on_delete=models.CASCADE)
+	id_skill = models.ForeignKey('Skill', on_delete=models.CASCADE)
+
+	cursus_users = models.ManyToManyField('CursusUser', through='CursusUserSkill', related_name='skills')
+
+# Creates a relation between a cursus skill and a cursus user
+class CursusUserSkill(models.Model):
+	id_cursus_skill = models.ForeignKey('CursusSkill', on_delete=models.CASCADE)
+	id_cursus_user = models.ForeignKey('CursusUser', on_delete=models.CASCADE)
+	level = models.FloatField()
