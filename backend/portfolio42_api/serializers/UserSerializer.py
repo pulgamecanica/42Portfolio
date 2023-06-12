ddbfd0e8939066be5449463702f44b4d42435a63
Diederik Boeren <diederik.boeren@gmail.com>
2023-06-12 16:28:16 +0100

    Moved Serializers that UserSerializer used into the file

--- backend/portfolio42_api/serializers/UserSerializer.py
+++ backend/portfolio42_api/serializers/UserSerializer.py
@@ -1,6 +1,20 @@
 from rest_framework import serializers
-from portfolio42_api.models import User
-from portfolio42_api.serializers import ProjectSerializer, CursusSerializer
+from portfolio42_api.models import User, Skill, Cursus, Project
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Skill
+        fields = ['name', 'intra_id']
+
+class CursusSerializer(serializers.ModelSerializer):
+    skills = SkillSerializer(many=True, read_only=True)
+    class Meta():
+        model = Cursus
+        fields = ['name', 'kind', 'intra_id', 'skills']
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Project
+        fields = ['name', 'description', 'exam', 'solo', 'intra_id']
 
 class UserSerializer(serializers.ModelSerializer):
     cursus = CursusSerializer(many=True, read_only=True)
