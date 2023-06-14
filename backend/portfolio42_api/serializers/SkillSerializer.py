8897afad84b9eab17cd9431f7f86405cf8b50ff5
Diederik Boeren <diederik.boeren@gmail.com>
2023-06-14 16:28:08 +0100

    Implemented SkillSerializer

--- backend/portfolio42_api/serializers/SkillSerializer.py
+++ backend/portfolio42_api/serializers/SkillSerializer.py
@@ -1,7 +1,13 @@
 from rest_framework import serializers
-from portfolio42_api.models import Skill
+from portfolio42_api.models import Skill, Cursus
+
+class CursusSerializer(serializers.ModelSerializer):
+    class Meta():
+        model = Cursus
+        fields = ['id', 'intra_id', 'name', 'kind']
 
 class SkillSerializer(serializers.ModelSerializer):
+    cursus = CursusSerializer(many=True, read_only=True)
     class Meta():
         model = Skill
-        fields = ['name', 'intra_id']
+        fields = ['id', 'intra_id', 'name', 'cursus']
