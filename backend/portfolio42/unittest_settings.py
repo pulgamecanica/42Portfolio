21e33651ed3e325a284c45b10d785a9a67740c09
pulgamecanica <pulgamecanica11@gmail.com>
2023-07-10 18:24:44 +0200

    Fix settings to pass CD-CI

--- backend/portfolio42/unittest_settings.py
+++ backend/portfolio42/unittest_settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
     'portfolio42_api',
 ]
 
@@ -123,3 +124,13 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# REST Framework Dictionary for handling REST API Framework global settings
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
