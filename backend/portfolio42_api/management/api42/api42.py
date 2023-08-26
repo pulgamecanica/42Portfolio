6ec13c598fb01271dab14a911bcbe6284dc22246
Diederik Boeren <diederik.boeren@gmail.com>
2023-08-26 20:28:02 +0100

    Refactored variable names

--- backend/portfolio42_api/management/api42/api42.py
+++ backend/portfolio42_api/management/api42/api42.py
@@ -6,73 +6,73 @@ from datetime import datetime, timedelta
 import logging
 
 class AuthApi42():
-    __token_url = 'https://api.intra.42.fr/oauth/token'
+    _TOKEN_URL = 'https://api.intra.42.fr/oauth/token'
 
     def __init__(self,
                  uid : str = os.environ.get('INTRA_UID'),
                  secret : str = os.environ.get('INTRA_SECRET'),
                  reqs_per_second : int = 2,
                  wait_for_limit : bool = False):
-        self.__uid = uid
-        self.__secret = secret
+        self._uid = uid
+        self._secret = secret
 
-        self.__token_expires = datetime(1,1,1)
-        self.__access_token = None
-        self.__reqs_per_second = reqs_per_second
-        self.__await_limit = wait_for_limit
-        self.__window = [] # Will store when a request expires (aka datetime.now() + 1 second)
+        self._token_expires = datetime(1,1,1)
+        self._access_token = None
+        self._reqs_per_second = reqs_per_second
+        self._await_limit = wait_for_limit
+        self._window = [] # Will store when a request expires (aka datetime.now() + 1 second)
 
     @property
     def uid(self):
-        return self.__uid
+        return self._uid
 
     @property
     def secret(self):
-        return self.__secret
+        return self._secret
 
     @secret.setter
     def secret(self, _secret : str):
-        self.__secret = _secret
+        self._secret = _secret
 
     @property
     def access_token(self):
-        return self.__access_token
+        return self._access_token
 
 
     @property
     def await_limit(self):
-        return self.__await_limit
+        return self._await_limit
 
     @await_limit.setter
     def await_limit(self, flag : bool):
-        self.__await_limit = flag
+        self._await_limit = flag
 
     # This function should be called each time the user wants to make a request
     @property
     def token(self):
-        if (self.__token_expires > datetime.now() and self.__access_token is not None):
+        if (self._token_expires > datetime.now() and self._access_token is not None):
             # removed timed-out requests from our window
-            self.__window = [t for t in self.__window if datetime.now() < t]
+            self._window = [t for t in self._window if datetime.now() < t]
 
             # If we hit the request limit wait until a slot in the window opens up
-            if (len(self.__window) >= self.__reqs_per_second):
-                if (self.__await_limit):
-                    sleep_time = self.__window[0] - datetime.now()
+            if (len(self._window) >= self._reqs_per_second):
+                if (self._await_limit):
+                    sleep_time = self._window[0] - datetime.now()
                     time.sleep(sleep_time.total_seconds())
                 else:
                     logging.error('Too Many Requests')
                     raise ApiException('Too many requests')
-            return self.__access_token
+            return self._access_token
 
         # If the token has expired or its not available try to refresh token
         data = {
             'grant_type': 'client_credentials',
-            'client_id': self.__uid,
-            'client_secret': self.__secret
+            'client_id': self._uid,
+            'client_secret': self._secret
         }
 
         # Fetch token
-        res = requests.post(AuthApi42.__token_url, data=data)
+        res = requests.post(AuthApi42._TOKEN_URL, data=data)
         json = res.json()
 
         # throw an exception if we encounter an error (non 200 response code)
@@ -85,33 +85,33 @@ class AuthApi42():
             raise RuntimeError(error_str)
 
         # Extract info from response
-        self.__access_token = json['access_token']
-        self.__token_expires = datetime.now() + timedelta(seconds=json['expires_in'])
+        self._access_token = json['access_token']
+        self._token_expires = datetime.now() + timedelta(seconds=json['expires_in'])
 
-        logging.info(f"Fetched new token, expires at {self.__token_expires}")
+        logging.info(f"Fetched new token, expires at {self._token_expires}")
 
         now = datetime.now()
 
         # removed timed-out requests from our window
-        self.__window = [t for t in self.__window if now < t]
+        self._window = [t for t in self._window if now < t]
 
-        if (len(self.__window) >= self.__reqs_per_second):
+        if (len(self._window) >= self._reqs_per_second):
             if (self.await_limit):
-                sleep_time = self.__window[0] - datetime.now()
+                sleep_time = self._window[0] - datetime.now()
                 time.sleep(sleep_time.total_seconds())
             else:
                 raise ApiException('Too many requests')
 
-        return self.__access_token
+        return self._access_token
 
     # Should be called after making a request,
     # updates the window for more accurate rate limit checking
     def report_request(self):
-        self.__window.append(datetime.now() + timedelta(seconds=1))
+        self._window.append(datetime.now() + timedelta(seconds=1))
 
 
 class Api42():
-    __api_base_url = "https://api.intra.42.fr"
+    _API_URL = "https://api.intra.42.fr"
 
     """
         Interface for interacting with the 42 api
@@ -121,17 +121,17 @@ class Api42():
         req_limit - The amount of requests that can be made per second
     """
     def __init__(self, uid :str, secret : str, req_limit : int = 2):
-        self.__auth = AuthApi42(uid, secret, reqs_per_second = req_limit, wait_for_limit= True)
+        self._auth = AuthApi42(uid, secret, reqs_per_second = req_limit, wait_for_limit= True)
 
     def await_limit(self, should_wait : bool):
-        self.__auth.await_limit(should_wait)
+        self._auth.await_limit(should_wait)
 
     def get(self, endpoint : str, params : dict = { }):
         # Make request
-        headers = {'Authorization': f"Bearer {self.__auth.token()}"}
-        res = requests.get(f"{Api42.__api_base_url}{endpoint}",
+        headers = {'Authorization': f"Bearer {self._auth.token}"}
+        res = requests.get(f"{Api42._API_URL}{endpoint}",
                            headers=headers, params=params)
-        self.__auth.report_request()
+        self._auth.report_request()
 
         logging.info(f"Made request to 42 API at {endpoint} ({res.status_code})")
 
