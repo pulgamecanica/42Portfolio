ba678ff7b37a9157294b6bc33aaf9af1a73dcb79
pulgamecanica <pulgamecanica11@gmail.com>
2023-08-29 12:50:26 +0200

    Added Search parameters to the Admin site

--- backend/portfolio42_api/admin.py
+++ backend/portfolio42_api/admin.py
@@ -1,8 +1,20 @@
 from django.contrib import admin
 from .models import User, Project, Skill, Cursus
 
+class UserAdmin(admin.ModelAdmin):
+	search_fields = ['intra_username', 'first_name', 'last_name', 'email', 'intra_id']
+
+class ProjectAdmin(admin.ModelAdmin):
+	search_fields = ['name', 'description', 'intra_id']
+
+class SkillAdmin(admin.ModelAdmin):
+	search_fields = ['name', 'intra_id']
+
+class CursusAdmin(admin.ModelAdmin):
+	search_fields = ['name', 'kind', 'intra_id']
+
 # Register your models here.
-admin.site.register(User)
-admin.site.register(Project)
-admin.site.register(Skill)
-admin.site.register(Cursus)
+admin.site.register(User, UserAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Skill, SkillAdmin)
+admin.site.register(Cursus, CursusAdmin)
