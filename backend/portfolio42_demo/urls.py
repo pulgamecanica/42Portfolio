0f1c47c8b49d77aa1091b62ec583ba3201957507
pulgamecanica <pulgamecanica11@gmail.com>
2023-08-31 11:39:47 +0200

    Cleanup

--- backend/portfolio42_demo/urls.py
+++ backend/portfolio42_demo/urls.py
@@ -1,8 +1,6 @@
 from django.urls import path
 from portfolio42_demo import views
 
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', views.home_page, name="home"),
     path('profile', views.profile, name='profile'),
