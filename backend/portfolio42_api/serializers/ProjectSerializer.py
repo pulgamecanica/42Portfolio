9356e354570cca8f9c0d6ea0b568995d59679da1
Diederik Boeren <diederik.boeren@gmail.com>
2023-12-27 08:38:38 +0000

    Fixed serializer to use translations

--- backend/portfolio42_api/serializers/ProjectSerializer.py
+++ backend/portfolio42_api/serializers/ProjectSerializer.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
-from portfolio42_api.models import Project, User, Cursus
+from portfolio42_api.models import Project, User, Cursus, ProjectTranslation
+import re
 
 class CursusSerializer(serializers.ModelSerializer):
     class Meta():
@@ -25,9 +26,28 @@ class ProjectSerializer(serializers.ModelSerializer):
     class Meta():
         model = Project
         fields = ['name',
-                  'description',
                   'exam',
                   'solo',
                   'intra_id',
                   'cursus',
                   'users']
+
+    def to_representation(self, instance):
+        instance_data = super().to_representation(instance)
+        lang_raw = self.context['request'].GET.get('lang','default')
+        langs = []
+        # lang_raw validation
+        if (re.fullmatch('^\[(?:[A-Z]+-[A-Z]+|default)(?:,(?:[A-Z]+-[A-Z]+|default))*\]$', lang_raw)):
+            langs = lang_raw[1:-1].split(',')
+        langs.append('default')
+        for lang in langs:
+            try:
+                pt = ProjectTranslation.objects.get(id_language__name_short=lang,id_project=instance)
+                print(f"found correct ${lang}")
+                instance_data['description'] = pt.description
+                instance_data['description_bonus'] = pt.description_bonus
+                instance_data['append_bonus'] = pt.bonus_append
+                break
+            except ProjectTranslation.DoesNotExist:
+                pass
+        return instance_data
